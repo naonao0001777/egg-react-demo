bb461be1b642462b597d62d2235ba9d71fe9a724
unknown <abi_wase1060@yahoo.co.jp>
2023-10-02 05:34:17 +0900

    helmet react

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,7 @@ function App() {
         <meta property="og:image" content="https://github.com/naonao0001777/egg-react/assets/46675984/7158844a-7e57-4c2e-9e31-6c9156e3e2da" />
         {/* OGP ここまで */}
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🐤</text></svg>"></link>
+        <meta name="twitter:card" content="summary" />
       </Helmet>
       <body className='has-background-primary-light is-unselectable max-width'>
         {/* has-background-dark */}
