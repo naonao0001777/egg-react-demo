e691ff8672efc48ea072bd70bcb182bf463b2444
unknown <abi_wase1060@yahoo.co.jp>
2023-10-01 19:59:38 +0900

    a little changed light mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import { css, keyframes } from '@emotion/react'
 import './css/mystyles.css'
 import logoIcon from './images/g8dyiy.jpg'
-import image1 from './images/2be646_27fb963ca56f44d2a44c9209a533d720-mv2 (1).jpg'
-import image2 from './images/20220720120530_1.jpg'
+import image1 from './images/wide.jpg'
+import image2 from './images/high.jpg'
 import { RiTwitterFill } from 'react-icons/ri'
 import { AiFillGithub } from 'react-icons/ai'
-import {RiTwitterXLine} from 'react-icons/ri'
+import { RiTwitterXLine } from 'react-icons/ri'
 import { IconContext } from 'react-icons'
 import { useEffect, useState, useRef } from "react"
 import { useClickOutside } from '@react-hooks-library/core'
+// import useSound from 'use-sound'
+import soundClick from './sounds/mouse-click.mp3'
 
 
 function App() {
@@ -26,19 +28,20 @@ function App() {
         </nav> */}
       </header>
       <body className='has-background-primary-light'>
+        {/* has-background-dark */}
         <div className='container'>
           <section className="section is-medium">
-            <div className='container is-flex is-justify-content-flex-start'>
+            <div className='is-flex is-justify-content-flex-start'>
               <figure className="image is-96x96 m-1">
                 <img src={logoIcon} className="is-rounded" alt="logo" />
               </figure>
               <div className='m-2'>
                 <h1 className="title">Egg</h1>
                 <h2 className="subtitle">
-                  <strong>Illustrator / Video Editor</strong>
+                  <strong className=''>Illustrator / Video Editor</strong>
                 </h2>
                 <IconContext.Provider value={{ size: '30px' }}>
-                  <a href='https://twitter.com/egg_is_blue' target="_blank" rel="noopener noreferrer">
+                  <a className='' href='https://twitter.com/egg_is_blue' target="_blank" rel="noopener noreferrer">
                     <RiTwitterFill />
                   </a>
                 </IconContext.Provider>
@@ -49,51 +52,23 @@ function App() {
             </div>
           </section>
           <section className="section is-medium">
-            <h1 className='title is-4'>Picup</h1>
-            <div className='container is-flex is-justify-content-center is-flex-wrap-wrap'>
+            <h1 className='title is-4 '>Album</h1>
+            <div className='is-flex is-flex-wrap-wrap is-justify-content-space-between'>
               <MainIllusts />
-              <div className='is-flex is-flex-direction-column is-justify-content-end mb-2'>
-                <div className='m-2'>
-                  <h1 className="subtitle">Main Picture</h1>
-                </div>
-                <div>
-                  <p>
-                    discription
-                  </p>
-                  <p>
-                    expample something
-                  </p>
-                </div>
-              </div>
               <MainIllusts />
-              <div className='is-flex is-flex-direction-column is-justify-content-end mb-2'>
-                <div className='m-2'>
-                  <h1 className="subtitle">Main Picture</h1>
-                </div>
-                <div className=''>
-                  <p>
-                    discription
-                  </p>
-                  <p>
-                    expample something
-                  </p>
-                </div>
+              <MainIllusts />
+              <div className='is-flex is-flex-wrap-wrap is-justify-content-space-between'>
+                <SubIllust />
               </div>
-            </div>
-          </section>
-          <section className="section is-medium">
-            <h1 className='title is-4'>sub picup</h1>
-            <div className='container is-flex is-justify-content-center is-flex-wrap-wrap'>
-              <SubIllust />
             </div>
           </section>
         </div>
-      </body >
-      <footer className='footer has-background-info'>
-        <div className="content has-text-right">
+      </body>
+      <footer className='footer has-background-info-dark'>
+        <div className="has-text-right">
           <p>
             {/* <AiFillGithub /> */}
-            <strong>created</strong> by <a href="https://twitter.com/nyaoisnao" target="_blank" rel="noopener noreferrer">nao</a>. with
+            by <a className="has-text-white-ter" href="https://twitter.com/nyaoisnao" target="_blank" rel="noopener noreferrer">nao</a> with<span> </span>
             <img src="https://bulma.io/images/bulma-logo.png" width="70" height="38" />
           </p>
         </div>
@@ -103,11 +78,12 @@ function App() {
 }
 
 
-// メインモーダルコンポーネント
+// ワイドモーダルコンポーネント
 const MainIllusts = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => { setShow(true) };
+  // const [play, { stop, pause }] = useSound(soundClick);
   const ref = useRef(null)
   useClickOutside(ref, () => {
     setShow(false)
@@ -116,9 +92,9 @@ const MainIllusts = () => {
 
   return (
     <>
-      <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-200x200">
-          <img src={logoIcon} className="" alt="logo" />
+      <div className='m-3' onClick={handleShow}>
+        <figure className="image-wide">
+          <img src={image1} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -127,7 +103,7 @@ const MainIllusts = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image1} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
@@ -137,7 +113,7 @@ const MainIllusts = () => {
   );
 }
 
-// サブモーダルコンポーネント
+// ロングモーダルコンポーネント
 const SubIllust = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -147,8 +123,8 @@ const SubIllust = () => {
     <>
       {/* 1 */}
       <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-128x128">
-          <img src={logoIcon} className="" alt="logo" />
+        <figure className="image-long">
+          <img src={image2} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -157,7 +133,7 @@ const SubIllust = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image2} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
@@ -165,8 +141,8 @@ const SubIllust = () => {
       ) : <></>}
       {/* 2 */}
       <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-128x128">
-          <img src={logoIcon} className="" alt="logo" />
+        <figure className="image-long ">
+          <img src={image2} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -175,7 +151,7 @@ const SubIllust = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image2} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
@@ -183,8 +159,8 @@ const SubIllust = () => {
       ) : <></>}
       {/* 3 */}
       <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-128x128">
-          <img src={logoIcon} className="" alt="logo" />
+        <figure className="image-long ">
+          <img src={image2} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -193,7 +169,7 @@ const SubIllust = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image2} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
@@ -201,8 +177,8 @@ const SubIllust = () => {
       ) : <></>}
       {/* 4 */}
       <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-128x128">
-          <img src={logoIcon} className="" alt="logo" />
+        <figure className="image-long ">
+          <img src={image2} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -211,7 +187,7 @@ const SubIllust = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image2} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
@@ -219,8 +195,8 @@ const SubIllust = () => {
       ) : <></>}
       {/* 5 */}
       <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-128x128">
-          <img src={logoIcon} className="" alt="logo" />
+        <figure className="image-long ">
+          <img src={image2} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -229,16 +205,16 @@ const SubIllust = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image2} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
         </div>
       ) : <></>}
-      {/* 6 */}
+      {/* 5 */}
       <div className='m-3' onClick={handleShow} >
-        <figure className="image-space is-128x128">
-          <img src={logoIcon} className="" alt="logo" />
+        <figure className="image-long ">
+          <img src={image2} className="" alt="logo" />
         </figure>
       </div>
       {/* モーダル */}
@@ -247,7 +223,7 @@ const SubIllust = () => {
           <div className="modal-background"></div>
           <div className='modal-content'>
             <p className="image">
-              <img src={logoIcon} alt="" />
+              <img src={image2} alt="" />
             </p>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
